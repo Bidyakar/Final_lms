759c03c351b91a563b28d60c60f4bbb681714c3f
Bidyakar Bhatta <123646425+Bidyakar@users.noreply.github.com>
2025-08-07 14:53:11 +0545

    Add files via upload

--- eb_lms.sql
+++ eb_lms.sql
@@ -1,20 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
--- http://www.phpmyadmin.net
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 27, 2014 at 03:09 PM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
+-- Generation Time: Jun 10, 2025 at 03:17 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `eb_lms`
@@ -26,8 +27,8 @@ SET time_zone = "+00:00";
 -- Table structure for table `book`
 --
 
-CREATE TABLE IF NOT EXISTS `book` (
-  `book_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `book` (
+  `book_id` int(11) NOT NULL,
   `book_title` varchar(100) NOT NULL,
   `category_id` int(50) NOT NULL,
   `author` varchar(50) NOT NULL,
@@ -38,19 +39,18 @@ CREATE TABLE IF NOT EXISTS `book` (
   `copyright_year` int(11) NOT NULL,
   `date_receive` varchar(20) NOT NULL,
   `date_added` datetime NOT NULL,
-  `status` varchar(30) NOT NULL,
-  PRIMARY KEY (`book_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
+  `status` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `book`
 --
 
 INSERT INTO `book` (`book_id`, `book_title`, `category_id`, `author`, `book_copies`, `book_pub`, `publisher_name`, `isbn`, `copyright_year`, `date_receive`, `date_added`, `status`) VALUES
-(15, 'Natural Resources', 8, 'Robin Kerrod', 15, 'Marshall Cavendish Corporation', 'Marshall', '1-85435-628-3', 1997, '', '2013-12-11 06:34:27', 'New'),
-(16, 'Encyclopedia Americana', 5, 'Grolier', 20, 'Connecticut', 'Grolier Incorporation', '0-7172-0119-8', 1988, '', '2013-12-11 06:36:23', 'Archive'),
-(17, 'Algebra 1', 3, 'Carolyn Bradshaw, Michael Seals', 35, 'Pearson Education, Inc', 'Prentice Hall, New Jersey', '0-13-125087-6', 2004, '', '2013-12-11 06:39:17', 'Damage'),
-(18, 'The Philippine Daily Inquirer', 7, '..', 3, 'Pasay City', '..', '..', 2013, '', '2013-12-11 06:41:53', 'New'),
+(15, 'Natural Resources', 8, 'Robin Kerrod', 15, 'Marshall Cavendish Corporation', 'Marshall', '1-85435-628-3', 1997, '', '2021-12-11 06:34:27', 'New'),
+(16, 'Sociology', 5, 'Management', 20, 'KCMIT', 'Tulsi Bastola', '0-7172-0119-8', 1988, '', '2018-12-11 06:36:23', 'Archive'),
+(17, 'Operation Management', 3, 'Carolyn Bradshaw, Michael Seals', 35, 'Pearson Education, Inc', 'Tulsi Bastola', '0-13-125087-6', 2006, '', '2013-12-11 06:39:17', 'Old'),
+(18, 'The Philippine Daily Inquirer', 7, '..', 3, 'Pasay City', '..', '..', 2013, '', '2013-12-11 06:41:53', 'Archive'),
 (19, 'Science in our World', 4, 'Brian Knapp', 25, 'Regency Publishing Group', 'Prentice Hall, Inc', '0-13-050841-1', 1996, '', '2013-12-11 06:44:44', 'Lost'),
 (20, 'Literature', 9, 'Greg Glowka', 20, 'Regency Publishing Group', 'Prentice Hall, Inc', '0-13-050841-1', 2001, '', '2013-12-11 06:47:44', 'Old'),
 (21, 'Lexicon Universal Encyclopedia', 5, 'Lexicon', 10, 'Lexicon Publication', 'Pulication Inc., Lexicon', '0-7172-2043-5', 1993, '', '2013-12-11 06:49:53', 'Old'),
@@ -62,9 +62,9 @@ INSERT INTO `book` (`book_id`, `book_title`, `category_id`, `author`, `book_copi
 (27, 'Asya Pag-usbong Ng Kabihasnan ', 8, 'Ricardo T. Jose, Ph . D.', 21, 'Vibal Publishing House Inc.', 'Araneta Avenue . Cor. Maria Clara St., Quezon City', '971-07-2324-3', 2008, '', '2013-12-11 07:02:56', 'New'),
 (28, 'Literature (the readers choice)', 9, 'Glencoe McGraw Hill', 20, '..', 'the McGrawHill Companies Inc', '0-02-817934-x', 2001, '', '2013-12-11 07:05:25', 'Damage'),
 (29, 'Beloved a Novel', 9, 'Toni Morrison', 13, '..', 'Alfred A. Knoff, Inc', '0-394-53597-9', 1987, '', '2013-12-11 07:07:02', 'Old'),
-(30, 'Silver Burdett Engish', 2, 'Judy Brim', 12, 'Silver Burdett Company', 'Silver', '0-382-03575-5', 1985, '', '2013-12-11 09:22:50', 'Old'),
-(31, 'The Corporate Warriors (Six Classic Cases in American Business)', 8, 'Douglas K. Ramsey', 8, 'Houghton Miffin Company', '..', '0-395-35487-0', 1987, '', '2013-12-11 09:25:32', 'Old'),
-(32, 'Introduction to Information System', 9, 'Cristine Redoblo', 10, 'CHMSC', 'Brian INC', '123-132', 2013, '', '2014-01-17 19:00:10', 'New');
+(30, 'Silver Burdett Engish', 2, 'Judy Brim', 12, 'Silver Burdett Company', 'Silver', '0-382-03575-5', 1985, '', '2013-12-11 09:22:50', 'Archive'),
+(31, 'The Corporate Warriors (Six Classic Cases in American Business)', 8, 'Douglas K. Ramsey', 8, 'Houghton Miffin Company', '..', '0-395-35487-0', 1987, '', '2013-12-11 09:25:32', 'Archive'),
+(32, 'Introduction to Information System', 9, 'Cristine Redoblo', 10, 'CHMSC', 'Brian INC', '123-132', 2013, '', '2014-01-17 19:00:10', 'Archive');
 
 -- --------------------------------------------------------
 
@@ -72,15 +72,12 @@ INSERT INTO `book` (`book_id`, `book_title`, `category_id`, `author`, `book_copi
 -- Table structure for table `borrow`
 --
 
-CREATE TABLE IF NOT EXISTS `borrow` (
-  `borrow_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `borrow` (
+  `borrow_id` int(11) NOT NULL,
   `member_id` bigint(50) NOT NULL,
   `date_borrow` varchar(100) NOT NULL,
-  `due_date` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`borrow_id`),
-  KEY `borrowerid` (`member_id`),
-  KEY `borrowid` (`borrow_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=485 ;
+  `due_date` varchar(100) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `borrow`
@@ -89,7 +86,13 @@ CREATE TABLE IF NOT EXISTS `borrow` (
 INSERT INTO `borrow` (`borrow_id`, `member_id`, `date_borrow`, `due_date`) VALUES
 (484, 55, '2014-03-20 23:50:27', '21/03/2014'),
 (483, 55, '2014-03-20 23:49:34', '21/03/2014'),
-(482, 52, '2014-03-20 23:38:22', '03/01/2014');
+(482, 52, '2014-03-20 23:38:22', '03/01/2014'),
+(485, 52, '2025-05-25 14:11:27', '29/05/2025'),
+(486, 52, '2025-05-25 14:11:36', '29/05/2025'),
+(487, 57, '2025-05-25 22:50:04', '28/05/2025'),
+(488, 58, '2025-05-26 18:03:14', '29/05/2025'),
+(489, 57, '2025-05-27 09:49:52', '30/05/2025'),
+(490, 54, '2025-05-31 14:54:43', '31/05/2025');
 
 -- --------------------------------------------------------
 
@@ -97,14 +100,13 @@ INSERT INTO `borrow` (`borrow_id`, `member_id`, `date_borrow`, `due_date`) VALUE
 -- Table structure for table `borrowdetails`
 --
 
-CREATE TABLE IF NOT EXISTS `borrowdetails` (
-  `borrow_details_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `borrowdetails` (
+  `borrow_details_id` int(11) NOT NULL,
   `book_id` int(11) NOT NULL,
   `borrow_id` int(11) NOT NULL,
   `borrow_status` varchar(50) NOT NULL,
-  `date_return` varchar(100) NOT NULL,
-  PRIMARY KEY (`borrow_details_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=165 ;
+  `date_return` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `borrowdetails`
@@ -113,7 +115,14 @@ CREATE TABLE IF NOT EXISTS `borrowdetails` (
 INSERT INTO `borrowdetails` (`borrow_details_id`, `book_id`, `borrow_id`, `borrow_status`, `date_return`) VALUES
 (164, 16, 484, 'pending', ''),
 (162, 15, 482, 'pending', ''),
-(163, 15, 483, 'returned', '2014-03-21 00:30:51');
+(163, 15, 483, 'Returned', '2025-05-26'),
+(165, 15, 485, 'pending', ''),
+(166, 15, 486, 'pending', ''),
+(167, 15, 487, 'Returned', '2025-05-26'),
+(168, 17, 487, 'pending', ''),
+(169, 17, 488, 'Returned', '2025-05-26'),
+(170, 21, 489, 'Returned', '2025-05-27'),
+(171, 19, 490, 'Returned', '2025-05-31');
 
 -- --------------------------------------------------------
 
@@ -121,13 +130,10 @@ INSERT INTO `borrowdetails` (`borrow_details_id`, `book_id`, `borrow_id`, `borro
 -- Table structure for table `category`
 --
 
-CREATE TABLE IF NOT EXISTS `category` (
-  `category_id` int(11) NOT NULL AUTO_INCREMENT,
-  `classname` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`category_id`),
-  UNIQUE KEY `category_id` (`category_id`),
-  KEY `classid` (`category_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=801 ;
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `classname` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `category`
@@ -142,7 +148,8 @@ INSERT INTO `category` (`category_id`, `classname`) VALUES
 (6, 'Filipiniana'),
 (7, 'Newspaper'),
 (8, 'General'),
-(9, 'References');
+(9, 'References'),
+(10, 'Management');
 
 -- --------------------------------------------------------
 
@@ -150,13 +157,12 @@ INSERT INTO `category` (`category_id`, `classname`) VALUES
 -- Table structure for table `lost_book`
 --
 
-CREATE TABLE IF NOT EXISTS `lost_book` (
-  `Book_ID` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `lost_book` (
+  `Book_ID` int(11) NOT NULL,
   `ISBN` int(11) NOT NULL,
   `Member_No` varchar(50) NOT NULL,
-  `Date Lost` date NOT NULL,
-  PRIMARY KEY (`Book_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `Date Lost` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -164,8 +170,8 @@ CREATE TABLE IF NOT EXISTS `lost_book` (
 -- Table structure for table `member`
 --
 
-CREATE TABLE IF NOT EXISTS `member` (
-  `member_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `member` (
+  `member_id` int(11) NOT NULL,
   `firstname` varchar(100) NOT NULL,
   `lastname` varchar(100) NOT NULL,
   `gender` varchar(10) NOT NULL,
@@ -173,28 +179,22 @@ CREATE TABLE IF NOT EXISTS `member` (
   `contact` varchar(100) NOT NULL,
   `type` varchar(100) NOT NULL,
   `year_level` varchar(100) NOT NULL,
-  `status` varchar(100) NOT NULL,
-  PRIMARY KEY (`member_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=66 ;
+  `status` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `member`
 --
 
 INSERT INTO `member` (`member_id`, `firstname`, `lastname`, `gender`, `address`, `contact`, `type`, `year_level`, `status`) VALUES
-(52, 'Mark', 'Sanchez', 'Male', 'Talisay', '212010', 'Teacher', 'Faculty', 'Active'),
-(53, 'April Joy', 'Aguilar', 'Female', 'E.B. Magalona', '00', 'Student', 'Second Year', 'Banned'),
-(54, 'Alfonso', 'Pancho', 'Male', 'E.B. Magalona', '009', 'Student', 'First Year', 'Active'),
-(55, 'Jonathan ', 'Antanilla', 'Male', 'E.B. Magalona', '0032', 'Student', 'Fourth Year', 'Active'),
-(56, 'Renzo Bryan', 'Pedroso', 'Male', 'Silay City', '03030', 'Student', 'Third Year', 'Active'),
-(57, 'Eleazar', 'Duterte', 'Male', 'E.B. Magalona', '90902', 'Student', 'Second Year', 'Active'),
-(58, 'Ellen Mae', 'Espino', 'Female', 'E.B. Magalona', '123', 'Student', 'First Year', 'Active'),
-(59, 'Ruth', 'Magbanua', 'Female', 'E.B. Magalona', '9340', 'Student', 'Second Year', 'Active'),
-(60, 'Shaina Marie', 'Gabino', 'Female', 'Silay City', '132134', 'Student', 'Second Year', 'Active'),
-(62, 'Chairty Joy', 'Punzalan', 'Female', 'E.B. Magalona', '12423', 'Teacher', 'Faculty', 'Active'),
-(63, 'Kristine May', 'Dela Rosa', 'Female', 'Silay City', '1321', 'Student', 'Second Year', 'Active'),
-(64, 'Chinie marie', 'Laborosa', 'Female', 'E.B. Magalona', '902101', 'Student', 'Second Year', 'Active'),
-(65, 'Ruby', 'Morante', 'Female', 'E.B. Magalona', '', 'Teacher', 'Faculty', 'Active');
+(52, 'Ram', 'Sharma', 'Male', 'Baneshwor', '2120245612', 'Teacher', 'Faculty', 'Active'),
+(53, 'Bidyakar', 'Bhatta', 'Male', 'Ghattaghar', '00', 'Student', 'Seven Semester', 'Banned'),
+(54, 'Sushil', 'Koirala', 'Male', 'Baneshwor', '009', 'Student', 'First Semester', 'Active'),
+(55, 'Sayog ', 'Rai', 'Male', 'Lalitpur', '0032', 'Student', 'Seven Semester', 'Active'),
+(57, 'Rohit', 'Sharma', 'Male', 'Mumbai', '90902', 'Student', 'Eighth Semester', 'Active'),
+(58, 'Sita', 'Karki', 'Female', 'Thimi', '1233457891', 'Student', 'First Year', 'Banned'),
+(67, 'Manoj', 'Thakur', 'Male', 'Lalitpur', '9876543210', 'Student', 'Third Year', 'Active'),
+(69, 'Pat', 'cummins', 'Male', 'sankhamul', '0123432678', 'Teacher', 'Faculty', 'Active');
 
 -- --------------------------------------------------------
 
@@ -202,13 +202,10 @@ INSERT INTO `member` (`member_id`, `firstname`, `lastname`, `gender`, `address`,
 -- Table structure for table `type`
 --
 
-CREATE TABLE IF NOT EXISTS `type` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `borrowertype` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `borrowertype` (`borrowertype`),
-  KEY `id` (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=42 ;
+CREATE TABLE `type` (
+  `id` int(11) NOT NULL,
+  `borrowertype` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `type`
@@ -227,21 +224,132 @@ INSERT INTO `type` (`id`, `borrowertype`) VALUES
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `firstname` varchar(100) NOT NULL,
-  `lastname` varchar(100) NOT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `lastname` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`user_id`, `username`, `password`, `firstname`, `lastname`) VALUES
-(2, 'admin', 'admin', 'john', 'smith');
+(2, 'admin', 'admin', 'Bidyakar', 'Bhatta'),
+(30, 'bb', 'bb', 'bb', 'bb');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`book_id`);
+
+--
+-- Indexes for table `borrow`
+--
+ALTER TABLE `borrow`
+  ADD PRIMARY KEY (`borrow_id`),
+  ADD KEY `borrowerid` (`member_id`),
+  ADD KEY `borrowid` (`borrow_id`);
+
+--
+-- Indexes for table `borrowdetails`
+--
+ALTER TABLE `borrowdetails`
+  ADD PRIMARY KEY (`borrow_details_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`),
+  ADD UNIQUE KEY `category_id` (`category_id`),
+  ADD KEY `classid` (`category_id`);
+
+--
+-- Indexes for table `lost_book`
+--
+ALTER TABLE `lost_book`
+  ADD PRIMARY KEY (`Book_ID`);
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`member_id`);
+
+--
+-- Indexes for table `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `borrowertype` (`borrowertype`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `book`
+--
+ALTER TABLE `book`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT for table `borrow`
+--
+ALTER TABLE `borrow`
+  MODIFY `borrow_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=491;
+
+--
+-- AUTO_INCREMENT for table `borrowdetails`
+--
+ALTER TABLE `borrowdetails`
+  MODIFY `borrow_details_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=172;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=801;
+
+--
+-- AUTO_INCREMENT for table `lost_book`
+--
+ALTER TABLE `lost_book`
+  MODIFY `Book_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `member`
+--
+ALTER TABLE `member`
+  MODIFY `member_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
+
+--
+-- AUTO_INCREMENT for table `type`
+--
+ALTER TABLE `type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
