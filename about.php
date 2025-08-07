f23eef0a310c7479343fcce82754cd234ffa8baa
Bidyakar Bhatta <123646425+Bidyakar@users.noreply.github.com>
2025-08-07 14:32:10 +0545

    Add files via upload

---
+++ about.php
@@ -0,0 +1,60 @@
+
+<?php include('header.php'); ?>
+<?php include('navbar_about.php'); ?>
+    <div class="container">
+		<div class="margin-top">
+			<div class="row">	
+			<?php include('head.php'); ?>
+				
+				<div class="text_content">
+					
+					<table width="450" style="margin: 0pt auto;">
+	
+	<thead><tr><th colspan="3" style="text-align: center;"><h3>LIBRARY HOURS</h3></th></tr></thead>
+	<tbody>
+	<tr>
+	<td colspan="3"></td>
+	</tr>
+	<tr>
+	<td>Sunday to Friday</td>
+	<td>8:00 a.m. to 12:30 a.m.</td>
+	</tr>
+	
+	</tbody>
+	</table>
+	
+	<table class="staff" >
+	<thead><tr><th colspan="3"  style="text-align: center;"><h3>LIBRARY STAFF</h3></th></tr></thead>
+	<tbody>
+	<tr>
+	<td colspan="3"></td>
+	</tr>
+	<tr>
+	<td>Mr. Sushil Koirala<br />Librarian </td>
+	</tr>
+	
+	</tbody>
+	</table>
+					
+					</div>
+					</div>
+			
+		</div>
+    </div>
+<?php include('footer.php') ?>
+<!-- Made by Vinit Shahdeo -->
+
+
+C:\Users\Priyanka>nmap -sn 192.168.1.0/24
+Starting Nmap 7.95 ( https://nmap.org ) at 2025-04-30 18:16 Nepal Standard Time
+Nmap scan report for 192.168.1.1
+Host is up (0.017s latency).
+Nmap scan report for 192.168.1.100
+Host is up (0.0100s latency).
+Nmap done: 256 IP addresses (2 hosts up) scanned in 32.00 seconds
+
+C:\Users\Priyanka>nmap -sn 192.168.18.75
+Starting Nmap 7.95 ( https://nmap.org ) at 2025-04-30 18:24 Nepal Standard Time
+Nmap scan report for 192.168.18.75
+Host is up.
+Nmap done: 1 IP address (1 host up) scanned in 0.20 seconds
